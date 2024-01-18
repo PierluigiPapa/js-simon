bda067a10282929ef495606d5d2dd09003bebe8e
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-18 16:56:49 +0100

    feat: add const days, hours, minutes, seconds

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+//Costante per stabiliare i giorni
+const daysHtml = document.querySelectorAll('days');
+//Costante per stabilire le ore
+const hoursHtml = document.querySelectorAll('hours');
+//Costante per stabilire i minuti
+const minutesHtml = document.querySelectorAll('minutes');
+//Costante per stabilire i secondi
+const secondsHtml = document.querySelectorAll('seconds');
