2c7975c020888067b3b93f0d05966d836a071f38
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-18 17:10:52 +0100

    feat: add two const

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,16 @@ const hoursHtml = document.querySelectorAll('hours');
 const minutesHtml = document.querySelectorAll('minutes');
 //Costante per stabilire i secondi
 const secondsHtml = document.querySelectorAll('seconds');
+//Costante per stabilire la data di oggi
+const dateToday = new Date().getFullYear();
+//Costnte per stabilire la data della lezione di domani
+const lessionsTime = new Date("2024-01-19 09:30:00");
+
+//Funzione per il countdown
+function updateCountdownLessions (){
+    const currentTime = new Date();
+    const diff = lessionsTime - currentTime;
+    console.log(diff);
+}
+
+updateCountdownLessions();
